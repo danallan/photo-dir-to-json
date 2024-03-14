a3ab6f54e48faa9933f15f4be6bcacdc714649fb
Daniel Armendariz <dan@a8z.space>
2024-03-14 11:02:17 -0700

    test: Add vitest and automated tests for Photo class

---
+++ test/photo.spec.ts
@@ -0,0 +1,115 @@
+import { describe, expect, test } from 'vitest';
+import { Photo } from '../src/photo.js';
+import { resolve } from 'path';
+
+const images = 'test/images';
+
+test('get path() returns absolute path', () => {
+    const jpg = new Photo(images, '1.jpg');
+    const path = resolve(`${images}/1.jpg`);
+    expect(jpg.path).toEqual(path);
+});
+
+test('get name() is correct', () => {
+    const jpg = new Photo(images, '1.jpg');
+    expect(jpg.name).toEqual('1.jpg');
+});
+
+['jpg', 'png', 'webp'].forEach((ext) => {
+    describe(`${ext.toUpperCase()} files`, () => {
+        test('loads file, metadata filename is correct', async () => {
+            const d = await (new Photo(images, `exif.${ext}`)).metadata();
+            expect(d.filename).toBe(`exif.${ext}`);
+        });
+
+        test('correctly parses image dimensions: landscape', async () => {
+            const d = await (new Photo(images, `wide.${ext}`)).metadata();
+            expect(d.width).toBe(90);
+            expect(d.height).toBe(30);
+            expect(d.landscape).toBe(true);
+        });
+
+        test('correctly parses image dimensions: square/portrait', async () => {
+            const d = await (new Photo(images, `exif.${ext}`)).metadata();
+            expect(d.width).toBe(16);
+            expect(d.height).toBe(16);
+            expect(d.landscape).toBe(false);
+        });
+
+        test('date uses EXIF creation time', async () => {
+            // `exiftool "-DateTimeOriginal=2023:01:01 00:00:01"`
+            const d = await (new Photo(images, `exif.${ext}`)).metadata();
+
+            // TZ unspecified, construct local time representation
+            const date = new Date(2023, 0, 1, 0, 0, 1);
+
+            expect(d.date).toBe(date.toISOString());
+        });
+
+    });
+});
+
+describe('date parsing', () => {
+    test('EXIF with OffsetTime is respected', async () => {
+        // `exiftool "-ExifIFD:OffsetTimeOriginal=+13:45" exif-offset.jpg`
+        // +13:45 is CHADT, Chatham Islands, NZ during DST
+        const d = await (new Photo(images, 'exif-offset.jpg')).metadata();
+
+        expect(d.date).toBe('2022-12-31T10:15:01.000Z');
+    });
+
+    test('EXIF with SubSecTime is respected', async () => {
+        // `exiftool "-ExifIFD:SubSecTimeOriginal=789" exif-subsec.jpg`
+        const d = await (new Photo(images, 'exif-subsec.jpg')).metadata();
+
+        // TZ unspecified, construct local time representation
+        const date = new Date(2023, 0, 1, 0, 0, 1, 789);
+
+        expect(d.date).toBe(date.toISOString());
+    });
+
+    test('XMP used when no EXIF', async () => {
+        // `exiftool "-XMP:CreateDate=2021-01-01T12:00:01.002+02:00" xmp.jpg`
+        // also applied to exif* files
+        const d = await (new Photo(images, 'xmp.jpg')).metadata();
+        expect(d.date).toBe('2021-01-01T10:00:01.002Z');
+    });
+
+    test('XMP missing time', async () => {
+        // `exiftool "-XMP:CreateDate=2021-01-01" xmp-no_time.jpg`
+        // also applied to exif* files
+        const d = await (new Photo(images, 'xmp-no_time.jpg')).metadata();
+
+        // TZ unspecified, construct local time representation
+        const date = new Date(2021, 0, 1, 0, 0, 0);
+
+        expect(d.date).toBe(date.toISOString());
+    });
+
+    test('IPTC used when no EXIF and no XMP', async () => {
+        // dates: `exiftool "-IPTC:DateCreated=20220101" iptc.jpg`
+        // times: `exiftool "-IPTC:TimeCreated=000001+0000" iptc.jpg`
+        // above also applied to exif.jpg and xmp.jpg
+        const d = await (new Photo(images, 'iptc.jpg')).metadata();
+        expect(d.date).toBe('2022-01-01T00:00:01.000Z');
+    });
+
+    test('IPTC missing TimeCreated', async () => {
+        // set date as above, removed time with:
+        // `exiftool "-IPTC:TimeCreated=" iptc-no_time.jpg`
+        const d = await (new Photo(images, 'iptc-no_time.jpg')).metadata();
+
+        // TZ unspecified, construct local time representation
+        const date = new Date(2022, 0, 1, 0, 0, 0);
+
+        expect(d.date).toBe(date.toISOString());
+    });
+
+
+    test('falls back to file creation time when no metadata', async() => {
+        // `SetFile -d '12/31/2023 16:00:01' no_date.jpg`
+        // on my utc-8 timezone machine. Verify with `stat -t %c`
+        const d = await (new Photo(images, 'no_date.jpg')).metadata();
+        expect(d.date).toBe('2024-01-01T00:00:01.000Z');
+    });
+});
