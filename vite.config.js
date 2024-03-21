cbd194562632fc879267967ca0fe131ccfbe0f9b
Daniel Armendariz <dan@a8z.space>
2024-03-21 13:58:24 -0700

    chore: Add some directories from gitignore in coverage exclusions

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     coverage: {
       reporter: ['text', 'json-summary', 'json'],
       reportOnFailure: true,
+      exclude: [
+        ...configDefaults.coverage.exclude,
+        'temp/**',
+        'build/**',
+      ],
     }
   }
 });
